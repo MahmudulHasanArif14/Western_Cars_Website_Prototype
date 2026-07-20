928f3eb903fe978153cb099e23f160216d4f5889
MahmudulHasanArif14 <arifmahmudulhasanlu84@gmail.com>
2026-07-20 16:28:21 +0100

    vercel speedinsights  added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Analytics />
+             <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
