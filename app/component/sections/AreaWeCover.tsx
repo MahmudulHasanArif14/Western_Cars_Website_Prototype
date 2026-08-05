f8096799907cae2f42a18fb39b6b7fb41ed5f110
MahmudulHasanArif14 <arifmahmudulhasanlu84@gmail.com>
2026-08-05 21:45:31 +0100

    updated the full homepages

--- app/component/sections/AreaWeCover.tsx
+++ app/component/sections/AreaWeCover.tsx
@@ -48,7 +48,7 @@ const itemVariants: Variants = {
     opacity: 1,
     y: 0,
     transition: {
-      type: "spring",
+      type: "spring" as const,
       stiffness: 300,
       damping: 25,
     },
